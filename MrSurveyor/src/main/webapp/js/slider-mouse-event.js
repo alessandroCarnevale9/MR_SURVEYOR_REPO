bddd44fce1d6ac08c575a7369b0f6d389edf7ed8
alessandroCarnevale9 <alecarnevale99@outlook.it>
2023-09-03 01:14:51 +0200

    Modified slider-mouse-event.js

--- MrSurveyor/src/main/webapp/js/slider-mouse-event.js
+++ MrSurveyor/src/main/webapp/js/slider-mouse-event.js
@@ -1,33 +1,33 @@
-const slides = document.querySelector(".slides");
-
 let moving = false;
 let mouseLastPosition = 0;
 let debounceInterval = 100; // Intervallo di debounce in millisecondi
 let debounceTimeout;
 
-slides.addEventListener("mousedown", (e) => {
-  e.preventDefault(); // Impedisce il trascinamento predefinito
-  moving = true;
-  mouseLastPosition = e.pageX;
-});
+allSlides.forEach((slides, i) => {
+  slides.addEventListener("mousedown", (e) => {
+    e.preventDefault(); // Impedisce il trascinamento predefinito
+    moving = true;
+    mouseLastPosition = e.pageX;
+  });
 
-slides.addEventListener("mousemove", (e) => {
-  if(moving) {
+  slides.addEventListener("mousemove", (e) => {
+    if(moving) {
 
-    clearTimeout(debounceTimeout);
-    debounceTimeout = setTimeout(() => {
-      
-      const diffX = e.pageX - mouseLastPosition;
+      clearTimeout(debounceTimeout);
+      debounceTimeout = setTimeout(() => {
 
-      if(diffX < 0) {
-        next();
-      }
-      else {
-        prev();
-      }
-    }, debounceInterval);
-  }
+        const diffX = e.pageX - mouseLastPosition;
 
+        if(diffX < 0) {
+          next(i+1);
+        }
+        else {
+          prev(i+1);
+        }
+      }, debounceInterval);
+    }
+
+  });
 });
 
 window.addEventListener("mouseup", () => {
